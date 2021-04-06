c781c30a69c485f1463bc15d58dc2b8ccf45aae8
Siajiza <>
2021-04-06 07:40:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# sentence = 'The quick brown fox jumped'
+# sentence -> variable
+# 'The quick brown fox jumped' -> string
+# = -> assignment
+
+sentence = 'The quick brown fox jumped'
+sentence_two = sentence.upper()
+
+print(sentence)
+print(sentence_two)
+
+sentence = 'the quick brown fox jumped'.capitalize()
+print(sentence)
+
+sentence = 'the quick brown fox jumped'.title()
+print(sentence)
+
+sentence = 'the Quick Brown fOx jumped'
+print(sentence.lower())
